110cbbdcb9f11fd83da5817a10795b47f8014d4c
AntoBrandi <antonio.brandi@outlook.it>
2023-06-21 22:19:56 +0200

    added code for last lesson

--- Section9_Build/arduinobot_ws/src/arduinobot_controller/launch/controller.launch.py
+++ Section9_Build/arduinobot_ws/src/arduinobot_controller/launch/controller.launch.py
@@ -1,12 +1,21 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
+from launch.conditions import UnlessCondition
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
+    is_sim = LaunchConfiguration('is_sim')
+    
+    is_sim_arg = DeclareLaunchArgument(
+        'is_sim',
+        default_value='True'
+    )
 
     robot_description = ParameterValue(
         Command(
@@ -17,6 +26,7 @@ def generate_launch_description():
                     "urdf",
                     "arduinobot.urdf.xacro",
                 ),
+                " is_sim:=False"
             ]
         ),
         value_type=str,
@@ -26,6 +36,22 @@ def generate_launch_description():
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}],
+        condition=UnlessCondition(is_sim),
+    )
+
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            {"robot_description": robot_description,
+             "use_sim_time": is_sim},
+            os.path.join(
+                get_package_share_directory("arduinobot_controller"),
+                "config",
+                "arduinobot_controllers.yaml",
+            ),
+        ],
+        condition=UnlessCondition(is_sim),
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -52,7 +78,9 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            is_sim_arg,
             robot_state_publisher_node,
+            controller_manager,
             joint_state_broadcaster_spawner,
             arm_controller_spawner,
             gripper_controller_spawner,
