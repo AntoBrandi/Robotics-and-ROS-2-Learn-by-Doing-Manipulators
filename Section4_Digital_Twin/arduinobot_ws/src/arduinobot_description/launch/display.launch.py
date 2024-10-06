e82de27f8ba0c0d7cc975e560c785dd5307a9a85
AntoBrandi <antonio.brandi@outlook.it>
2024-10-06 22:40:50 +0200

    update section 4

--- Section4_Digital_Twin/arduinobot_ws/src/arduinobot_description/launch/display.launch.py
+++ Section4_Digital_Twin/arduinobot_ws/src/arduinobot_description/launch/display.launch.py
@@ -10,33 +10,33 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    arduinobot_description_dir = get_package_share_directory('arduinobot_description')
+    arduinobot_description_dir = get_package_share_directory("arduinobot_description")
 
-    model_arg = DeclareLaunchArgument(name='model', default_value=os.path.join(
-                                        arduinobot_description_dir, 'urdf', 'arduinobot.urdf.xacro'
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        arduinobot_description_dir, "urdf", "arduinobot.urdf.xacro"
                                         ),
-                                      description='Absolute path to robot urdf file')
+                                      description="Absolute path to robot urdf file")
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
                                        value_type=str)
 
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
     )
 
     joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui'
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
     )
 
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', os.path.join(arduinobot_description_dir, 'rviz', 'display.rviz')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", os.path.join(arduinobot_description_dir, "rviz", "display.rviz")],
     )
 
     return LaunchDescription([
--- Section4_Digital_Twin/arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
+++ Section4_Digital_Twin/arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
@@ -12,56 +12,56 @@ from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    arduinobot_description = get_package_share_directory('arduinobot_description')
+    arduinobot_description = get_package_share_directory("arduinobot_description")
 
-    model_arg = DeclareLaunchArgument(name='model', default_value=os.path.join(
-                                        arduinobot_description, 'urdf', 'arduinobot.urdf.xacro'
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        arduinobot_description, "urdf", "arduinobot.urdf.xacro"
                                         ),
-                                      description='Absolute path to robot urdf file'
+                                      description="Absolute path to robot urdf file"
     )
 
     gazebo_resource_path = SetEnvironmentVariable(
-        name='GZ_SIM_RESOURCE_PATH',
+        name="GZ_SIM_RESOURCE_PATH",
         value=[
             str(Path(arduinobot_description).parent.resolve())
             ]
         )
     
-    ros_distro = os.environ['ROS_DISTRO']
-    physics_engine = '' if ros_distro == 'humble' else '--physics-engine gz-physics-bullet-featherstone-plugin'
+    ros_distro = os.environ["ROS_DISTRO"]
+    physics_engine = "" if ros_distro == "humble" else "--physics-engine gz-physics-bullet-featherstone-plugin"
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
                                        value_type=str)
 
     robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description,
-                     'use_sim_time': True}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
     )
 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
                 launch_arguments=[
-                    ('gz_args', [' -v 4 -r empty.sdf ', physics_engine]
+                    ("gz_args", [" -v 4 -r empty.sdf ", physics_engine]
                     )
                 ]
              )
 
     gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-topic', 'robot_description',
-                   '-name', 'arduinobot'],
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "arduinobot"],
     )
 
     gz_ros2_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
         arguments=[
-            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
         ]
     )
 
