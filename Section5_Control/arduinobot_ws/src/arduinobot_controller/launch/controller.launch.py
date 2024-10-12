182f4a7e7d8949e647b1af04e6609fc55960a474
AntoBrandi <antonio.brandi@outlook.it>
2024-10-12 17:12:26 +0200

    start rsp only if is sim

--- Section5_Control/arduinobot_ws/src/arduinobot_controller/launch/controller.launch.py
+++ Section5_Control/arduinobot_ws/src/arduinobot_controller/launch/controller.launch.py
@@ -1,5 +1,8 @@
 import os
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import UnlessCondition
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
@@ -7,6 +10,13 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+
+    is_sim_arg = DeclareLaunchArgument(
+        "is_sim",
+        default_value="True"
+    )
+
+    is_sim = LaunchConfiguration("is_sim")
 
     robot_description = ParameterValue(
         Command(
@@ -25,6 +35,7 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
+        condition=UnlessCondition(is_sim),
         parameters=[{"robot_description": robot_description}],
     )
 
@@ -52,6 +63,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            is_sim_arg,
             robot_state_publisher_node,
             joint_state_broadcaster_spawner,
             arm_controller_spawner,
